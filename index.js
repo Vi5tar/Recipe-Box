f41d6a58bf599ea952791cf35cd6e3c02a4d83cd
Travis Swiers <tswiers85@gmail.com>
2017-10-05 16:24:07 -0600

    Improved "Recipe Card" Population
    Using map() and child components as opposed to a for loop and
    dangerouslySetInnerHTML

--- index.js
+++ index.js
@@ -8,6 +8,39 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+var RecipeCard = function RecipeCard(props) {
+  return React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "div",
+      { className: "card" },
+      React.createElement(
+        "div",
+        { className: "card-header", role: "tab", id: "heading" + props.index },
+        React.createElement(
+          "h5",
+          { className: "mb-0" },
+          React.createElement(
+            "a",
+            { "data-toggle": "collapse", href: "#collapse" + props.index, "aria-expanded": "true", "aria-controls": "collapse" + props.index },
+            props.name
+          )
+        )
+      ),
+      React.createElement(
+        "div",
+        { id: "collapse" + props.index, className: "collapse", role: "tabpanel", "aria-labelledby": "heading" + props.index, "data-parent": "#accordion" },
+        React.createElement(
+          "div",
+          { className: "card-body" },
+          props.ingredients
+        )
+      )
+    )
+  );
+};
+
 var RecipeBox = function (_React$Component) {
   _inherits(RecipeBox, _React$Component);
 
@@ -29,32 +62,15 @@ var RecipeBox = function (_React$Component) {
       }]
     };
     _this.test = _this.test.bind(_this);
-    _this.createMarkup = _this.createMarkup.bind(_this);
-
     return _this;
   }
 
   _createClass(RecipeBox, [{
     key: "test",
     value: function test() {
-      console.log(this.state.recipes);
       var abc = this.state.recipes;
       abc.push({ name: "basic", ingredients: "bread and water" });
       this.setState({ abc: abc });
-      console.log(this.state.recipes);
-    }
-  }, {
-    key: "createMarkup",
-    value: function createMarkup() {
-      var cards = "";
-
-      for (var i = 0; i < this.state.recipes.length; i++) {
-        cards += "<div class='card'><div class='card-header' role='tab' id='heading" + i + "'><h5 class='mb-0'><a data-toggle='collapse' href='#collapse" + i + "' aria-expanded='true' aria-controls='collapse" + i + "'>" + this.state.recipes[i].name + "</a></h5></div><div id='collapse" + i + "' class='collapse' role='tabpanel' aria-labelledby='heading" + i + "' parent='#accordion'><div class='card-body'>" + this.state.recipes[i].ingredients + "</div></div></div>";
-      }
-
-      cards += "";
-
-      return { __html: cards };
     }
   }, {
     key: "render",
@@ -73,7 +89,13 @@ var RecipeBox = function (_React$Component) {
               { onClick: this.test },
               "test"
             ),
-            React.createElement("div", { id: "accordion", role: "tablist", dangerouslySetInnerHTML: this.createMarkup() })
+            React.createElement(
+              "div",
+              { id: "accordion", role: "tablist" },
+              this.state.recipes.map(function (recipes, index) {
+                return React.createElement(RecipeCard, { name: recipes.name, ingredients: recipes.ingredients, index: index });
+              })
+            )
           )
         )
       );
